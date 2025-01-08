9077b3495c4ef2cae17038785aef4a8d08074809
MR99924 <matthew.rodger@lgim.com>
2025-01-08 14:56:54 +0000

    First commit

--- Projects/EM_RatesRF/Sample.py
+++ Projects/EM_RatesRF/Sample.py
@@ -1,4 +1,3 @@
-print("Hello World")
-print("I am fun relaxed and easygoing")
-
-why in the name of what is honest and true can these things never just be simple and easy to use?
+print("\nHello World")
+print("War is God")
+pr
